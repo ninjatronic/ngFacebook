b666091b9ea9b469ce1ac8d04dd1c845b24d5723
Pete Martin <pete@changinghorizon.co.uk>
2013-09-08 15:56:57 +0100

    travis also builds (although no artifacts), build does not test

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,6 +106,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-strip');
 
     grunt.registerTask('test', ['jshint', 'karma:jasmine']);
-    grunt.registerTask('build', ['jshint', 'karma:jasmine', 'clean:temp', 'strip', 'concat', 'uglify:src', 'clean:build', 'copy:build', 'clean:temp']);
+    grunt.registerTask('build', ['clean:temp', 'strip', 'concat', 'uglify:src', 'clean:build', 'copy:build', 'clean:temp']);
     grunt.registerTask('default', ['watch:unit']);
 };
